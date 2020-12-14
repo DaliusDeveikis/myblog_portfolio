2873080c2493c62e8d4144d795357c13a97e2dfb
Dalius Deveikis <slkyper@gmail.com>
2020-12-14 20:41:28 +0200

    Added some code,new files.

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    tittle = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)

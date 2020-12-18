cafc02778e3a5981b6dcab091f3b3e6567bee20a
DaliusDeveikis <slkyper@gmail.com>
2020-12-18 08:32:59 +0000

    Removed old files

--- myblog_portfolio/settings.py
+++ myblog_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'r9(6^cv$c_wlvop+h#woo-2idsks%)ia_bbwub29v$7ljr+*j+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['idalux.pythonanywhere.com']
 
 
 # Application definition
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'portfolio',
-    'imagekit',
     'aboutme',
 ]
 
@@ -123,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
